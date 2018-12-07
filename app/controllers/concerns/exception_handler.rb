0d00d78fd6a715dd557b3ed7225d5403f8b761bd
Eleyni <eleyni.m.rodriguez@gmail.com>
2018-12-07 15:49:08 -0500

    Adds JWT and Api request authorization.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,50 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
-  included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
+  # Define custom error subclasses - rescue catches `StandardErrors`
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+  class ExpiredSignature < StandardError; end
+  class DecodeError < StandardError; end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
+  included do
+      # Define custom handlers
+      rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+      rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+      rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
+      rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+      rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
+      rescue_from ExceptionHandler::DecodeError, with: :four_zero_one
+
+      rescue_from ActiveRecord::RecordNotFound do |e|
+          json_response({ message: e.message }, :not_found)
+      end
+
+      rescue_from ActiveRecord::RecordInvalid do |e|
+        json_response({ message: e.message }, :unprocessable_entity)
+      end
+  end
+
+  private
+
+  # JSON response with message; Status code 422 - unprocessable_entity
+  def four_twenty_two(e)
       json_response({ message: e.message }, :unprocessable_entity)
-    end
+  end
+
+  # JSON response with Status code 401 - Unauthorized
+  def four_ninety_eight(e)
+    render json: { message: e.message }, status: :invalid_token
+  end
+
+  # JSON response with Status code 401 - Unauthorized
+  def four_zero_one(e)
+    render json: { message: e.message }, status: :invalid_token
+  end
+
+   # JSON response with Status code 401 - Unauthorized
+  def unauthorized_request(e)
+    render json: { message: e.message }, status: :unauthorized
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_171029) do
+ActiveRecord::Schema.define(version: 2018_12_06_191746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,14 @@ ActiveRecord::Schema.define(version: 2018_11_30_171029) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["school_id"], name: "index_tracks_on_school_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "courses", "schools"
